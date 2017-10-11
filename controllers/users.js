688696de9efdbea87361fcf929d72b97d631f2f7
Niraj Karki <niraj.karki@f1soft.com>
2017-10-11 15:39:31 +0545

    Section 4 end

--- controllers/users.js
+++ controllers/users.js
@@ -7,6 +7,13 @@ module.exports = function(_, passport, User){
 			router.get('/', this.indexPage);
 			router.post('/', User.LoginValidation, this.postLogin);
 			router.get('/signup', this.getSignUp);
+			//facebook login
+			router.get('/auth/facebook', this.getFacebookLogin);
+			router.get('/auth/facebook/callback', this.facebookLogin);
+			//google login
+			router.get('/auth/google', this.getGoogleLogin);
+			router.get('/auth/google/callback', this.googleLogin);
+
 			router.post('/signup', User.SignUpValidation, this.postSignUp);
 
 			router.get('/home', this.homePage);
@@ -25,6 +32,22 @@ module.exports = function(_, passport, User){
 			return res.render('signup', { title: 'ChatApp | Signup', messages: errors, hasErrors: errors.length > 0});
 		},
 		postSignUp: passport.authenticate('local.signup',{
+			successRedirect: '/home',
+			failureRedirect: '/signup',
+			failureFlash: true
+		}),
+		getFacebookLogin: passport.authenticate('facebook', {
+			scope: 'email'
+		}),
+		facebookLogin: passport.authenticate('facebook', {
+			successRedirect: '/home',
+			failureRedirect: '/signup',
+			failureFlash: true
+		}),
+		getGoogleLogin: passport.authenticate('google', {
+			scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/plus.profile.emails.read']
+		}),
+		googleLogin: passport.authenticate('google', {
 			successRedirect: '/home',
 			failureRedirect: '/signup',
 			failureFlash: true
--- models/user.js
+++ models/user.js
@@ -2,15 +2,14 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt-nodejs');
 
 const userSchema = mongoose.Schema({
-	username: {type: String, unique: true},
+	username: {type: String, default: ''},
 	fullname: {type: String, default: ''},
 	email: {type: String, unique: true},
-	password: {type: String, unique: true, default: ''},
+	password: {type: String, default: ''},
 	userImage: {type: String, default: 'default.jpg'},
 	facebook: {type: String, default: ''},
 	fbTokens: Array,
-	google: {type: String, default: ''},
-	googleTokens: Array
+	google: {type: String, default: ''}
 });
 
 userSchema.methods.encryptPassword = function(password){
